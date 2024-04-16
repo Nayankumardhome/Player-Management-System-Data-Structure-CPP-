3b7e94c95d74ad502c30cda059ef40208d4526b7
Nayankumardhome <99061764+Nayankumardhome@users.noreply.github.com>
2024-04-16 09:01:55 +0530

    Add files via upload

---
+++ Ds project linked list/Node.cpp
@@ -0,0 +1,38 @@
+#include "Node.h"
+
+	//Constructer
+	template<class T>
+	Node<T>::Node(T d){
+		this->prev = nullptr;
+		this->data = d;
+		this->next = nullptr;
+	}
+	
+	//Muteters/Setters
+	template<class T>
+	void Node<T>::setPrev(Node* p){
+		this->prev = p;
+	}
+	template<class T>
+	void Node<T>::setData(T d){
+		this->data = d;
+	}
+	template<class T>
+	void Node<T>::setNext(Node* n){
+		this->next = n;
+	}
+	
+	
+	//Inspecters/Accessers/Getters
+	template<class T>
+	Node<T>* Node<T>::getPrev(){
+		return this->prev;
+	}
+	template<class T>
+	T& Node<T>::getData(){
+		return this->data;
+	}
+	template<class T>
+	Node<T>* Node<T>::getNext(){
+		return this->next;
+	}
